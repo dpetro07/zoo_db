2a5019d8aba42b6c3bacd01bd884763f46ea8118
Daniel Petro <DanielPetro@nbp-69-228.nbp.ruw.rutgers.edu>
2016-01-27 16:56:04 -0500

    added an index.js file

---
+++ index.js
@@ -0,0 +1,13 @@
+var connection = function connection(){
+  host: ‘nameOfYourHost’,
+  user: ‘YourUserName’,
+  password: ‘YourPassword’,
+  database: ‘zoo_db’;
+}
+
+
+
+
+var mysql` = function require('mysql'){
+
+}
